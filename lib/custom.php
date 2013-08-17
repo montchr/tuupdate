c92c359e774b1bf6dd342114ad3542a01e36aa89
Chris Montgomery <mont.chr@gmail.com>
2013-08-17 12:57:49 -0400

    sidebar widgets...
    …are not actually widgets. they're not editable through wordpress. but they look like widgets.
    
    ideally, filter the output of pre-existing widgets to style them.
    
    - function tuu_widget_static() takes $name $slug and $content params
    
    - the sidebar is the same on every page, so make it so

--- lib/custom.php
+++ lib/custom.php
@@ -2,3 +2,25 @@
 /**
  * Custom functions
  */
+
+/**
+ * Returns HTML for sidebar with pane object classes.
+ * @param  string $name    The title of the widget, to be displayed above the content
+ * @param  string $slug    The lowercase namespace for CSS classes
+ * @param  string $content Formatted HTML or PHP to print as content
+ *
+ * @author Chris Montgomery
+ */
+function tuu_widget_static($name, $slug, $content) {
+	$slug = "widget--" . $slug;
+
+	?>
+	<div class="<?php echo $slug; ?>  widget--static  widget  pane">
+		<div class="<?php echo $slug; ?>__header  widget__header  pane__header  pane__header--beta">
+			<h3 class="<?php echo $slug; ?>__header__title  widget__header__title  pane__header--beta__title  pane__header__title"><?php echo $name; ?></h3>
+		</div>
+		<div class="<?php echo $slug; ?>__content  widget__content  pane__content">
+			<?php echo $content; ?>
+		</div>
+	</div><!-- end <?php echo "." . $slug; ?> -->
+<?php }
--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,1 +1,28 @@
-<?php dynamic_sidebar('sidebar-primary'); ?>
+<?php // dynamic_sidebar('sidebar-primary'); ?>
+
+
+
+
+
+
+
+<?php
+
+$content_watch = "
+	<p>Watch Temple Update on TUTV: Comcast channel 50 and Verizon channel 45 within Philadelphia city limits, or online anywhere.</p>
+	<p>New episodes air live at 11:30 a.m. and re-air at 7:30 p.m. every Thursday. Catch past episodes every day at 10 a.m. and 6 p.m.</p>
+";
+
+$content_weather = "
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur, quae, obcaecati a incidunt assumenda sapiente quidem iusto ex reprehenderit accusamus molestias nihil est sit unde eum fuga iure labore placeat!</p>
+";
+
+$content_social = "
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, veritatis, laborum perspiciatis rem saepe eius ea fugit commodi rerum consequuntur pariatur distinctio adipisci ex corporis porro dolores nisi repellat asperiores.</p>
+";
+
+tuu_widget_static('Where To Watch', 'watch', $content_watch);
+tuu_widget_static('WeatherWatch', 'weather', $content_weather);
+tuu_widget_static('Follow Us', 'social', $content_social);
+
+?>
