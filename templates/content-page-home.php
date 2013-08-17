fc15f5019a079a9b06da7a750699c1da82ce7feb
Chris Montgomery <mont.chr@gmail.com>
2013-08-17 11:56:03 -0400

    pane testing
    - define colors for categories (unemployed)

---
+++ templates/content-page-home.php
@@ -0,0 +1,8 @@
+<section class="pane  pane--cut">
+	<header class="pane__header--alpha  pane__header">
+		<h1 class="pane__header--alpha__title  pane__header__title">Title</h1>
+	</header>
+	<div class="pane__content  pane--cut__content">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, odit, quidem, totam eveniet temporibus repellendus commodi voluptas consectetur id ducimus molestiae fugit sapiente sed quasi accusantium numquam cumque incidunt maxime?
+	</div>
+</section>
---
+++ templates/sidebar-page-home.php
@@ -0,0 +1,8 @@
+<div class="pane">
+	<div class="pane__header pane__header--beta">
+		<h3 class="pane__header__title pane__header--beta__title">Another Title</h3>
+	</div>
+	<div class="pane__content">
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, dolor accusamus distinctio sequi praesentium sunt blanditiis temporibus ex assumenda? Ipsam modi aut ratione. Dolorum, necessitatibus veritatis ipsam numquam dolores quia.
+	</div>
+</div>
