1597879b821453ab730b763ae0eb24a28c6fc135
Chris Montgomery <mont.chr@gmail.com>
2013-08-17 21:37:23 -0400

    colophon page template

--- templates/content-page-colophon.php
+++ templates/content-page-colophon.php
@@ -1,26 +1,43 @@
-# Frameworks
+<section class="pane  pane--cut">
+	<header class="pane__header--alpha  pane__header">
+		<h1 class="pane__header--alpha__title  pane__header__title">Colophon</h1>
+	</header>
+	<div class="pane__content  pane--cut__content">
 
-WordPress
-Roots
-Exai
-inuit.css
+			<h3 id="frameworks">Frameworks</h3>
 
-# Typography
+				<ul>
+					<li>WordPress</li>
+					<li>Roots</li>
+					<li>Exai</li>
+					<li>inuit.css</li>
+				</ul>
 
-Proxima Nova
+			<h3 id="typography">Typography</h3>
 
-Typekit
+				<h4>Font</h4>
 
-# Icons
+					<p>Proxima Nova</p>
 
-IcoMoon
-Icon Minia
-Iconic
+				<h4>Provider</h4>
 
-# Design
+					<p>Typekit</p>
 
-Bella Pezzati
+			<h3 id="icons">Icons</h3>
 
-# Development
+				<ul>
+					<li>IcoMoon</li>
+					<li>Icon Minia</li>
+					<li>Iconic</li>
+				</ul>
 
-Chris Montgomery
+			<h3 id="design">Design</h3>
+
+				<p>Bella Pezzati</p>
+
+			<h3 id="development">Development</h3>
+
+				<p>Chris Montgomery</p>
+
+	</div>
+</section>
