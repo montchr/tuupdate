cd00d4e8efccdf9067f2da6cdfead4f50389ddd2
Chris Montgomery <mont.chr@gmail.com>
2013-08-18 19:22:47 -0400

    add custom post type "alert"

--- lib/init.php
+++ lib/init.php
@@ -27,3 +27,47 @@ add_action('after_setup_theme', 'exai_setup');
 
 // Backwards compatibility for older than PHP 5.3.0
 if (!defined('__DIR__')) { define('__DIR__', dirname(__FILE__)); }
+
+/**
+ * Custom post type: Alert
+ */
+function tuu_post_type_alert() {
+
+	$labels = array(
+		'name'                => _x( 'Alerts', 'Post Type General Name', 'exai' ),
+		'singular_name'       => _x( 'Alert', 'Post Type Singular Name', 'exai' ),
+		'menu_name'           => __( 'Alert', 'exai' ),
+		'parent_item_colon'   => __( 'Parent Alert:', 'exai' ),
+		'all_items'           => __( 'All Alerts', 'exai' ),
+		'view_item'           => __( 'View Alert', 'exai' ),
+		'add_new_item'        => __( 'Add New Alert', 'exai' ),
+		'add_new'             => __( 'New Alert', 'exai' ),
+		'edit_item'           => __( 'Edit Alert', 'exai' ),
+		'update_item'         => __( 'Update Alert', 'exai' ),
+		'search_items'        => __( 'Search alerts', 'exai' ),
+		'not_found'           => __( 'No alerts found', 'exai' ),
+		'not_found_in_trash'  => __( 'No alerts found in Trash', 'exai' ),
+	);
+	$args = array(
+		'label'               => __( 'alert', 'exai' ),
+		'description'         => __( 'A message box, usually appearing at the top of a page.', 'exai' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'excerpt', 'thumbnail', 'custom-fields', ),
+		'taxonomies'          => array( 'category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => false,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 20,
+		'menu_icon'           => '',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'post',
+	);
+	register_post_type( 'alert', $args );
+}
+add_action( 'init', 'tuu_post_type_alert', 0 );
