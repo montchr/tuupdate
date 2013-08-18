4434fe760389dafcde8e0f7606e274fe87aa9cb2
Chris Montgomery <mont.chr@gmail.com>
2013-08-17 23:03:37 -0400

    dropdown toggles will go nowhere, don't even try

--- lib/nav.php
+++ lib/nav.php
@@ -32,7 +32,7 @@ class Roots_Nav_Walker extends Walker_Nav_Menu {
 		// Remove depth dependency for dropdown toggle
 		// montchr 2013-07-25
 		if ($item->is_dropdown) {
-			$item_html = str_replace('<a', '<a class="dropdown__toggle" data-toggle="dropdown" data-target="#"', $item_html);
+			$item_html = str_replace('<a', '<a href="#" class="dropdown__toggle" data-toggle="dropdown" data-target="#"', $item_html);
 		}
 		elseif (stristr($item_html, 'li class="divider')) {
 			$item_html = preg_replace('/<a[^>]*>.*?<\/a>/iU', '', $item_html);
