210d249375df1baecf589337a6d34975eb944865
Chris Montgomery <mont.chr@gmail.com>
2013-08-18 21:41:09 -0400

    introduce short pane
    a short pane is for main headers and alerts
    
    also, pane comments

--- templates/page-header.php
+++ templates/page-header.php
@@ -1,5 +1,5 @@
-<header class="page-header--<?php echo tuu_get_page_slug(); ?>  page-header">
-	<div class="page-header--<?php echo tuu_get_page_slug(); ?>__inner  page-header__inner">
+<header class="page-header--<?php echo tuu_get_page_slug(); ?>  page-header  pane--short">
+	<div class="page-header--<?php echo tuu_get_page_slug(); ?>__inner  page-header__inner  pane--short__inner">
 		<h1 class="page-header--<?php echo tuu_get_page_slug(); ?>__title  page-header__title  page-title  h--main"><?php echo exai_title(); ?></h1>
 	</div>
 </header>
