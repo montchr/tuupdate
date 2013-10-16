8aa6dc42d7bfbaa920e814d93534d9422c8d1304
Chris Montgomery <mont.chr@gmail.com>
2013-10-16 14:21:20 -0400

    don't use advanced excerpt plugin "read more". fixes #40

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -44,7 +44,7 @@ foreach ($categories as $category) :
 					</header>
 
 					<div class="home__recent-post__content">
-						<?php the_excerpt(); // the_content( 'Continue reading &#187;' ); ?>
+						<?php the_excerpt(); echo exai_excerpt_more(); ?>
 					</div>
 
 				</article>
