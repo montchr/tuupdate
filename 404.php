fbc1ceb34572ac55880c3301a65343248c6eb6bc
Chris Montgomery <mont.chr@gmail.com>
2013-10-23 14:23:31 -0400

    cut panes on 404 and index, fixes #49

--- 404.php
+++ 404.php
@@ -1,13 +1,18 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert">
-	<?php _e('Sorry, but the page you were trying to view does not exist.', 'exai'); ?>
+<div class="pane pane--cut">
+    <div class="pane--cut__content  pane__content">
+
+        <div class="alert">
+        	<?php _e('Sorry, but the page you were trying to view does not exist.', 'exai'); ?>
+        </div>
+
+        <p><?php _e('It looks like this was the result of either:', 'exai'); ?></p>
+        <ul>
+            <li><?php _e('a mistyped address', 'exai'); ?></li>
+            <li><?php _e('an out-of-date link', 'exai'); ?></li>
+        </ul>
+
+        <?php get_search_form(); ?>
+    </div>
 </div>
-
-<p><?php _e('It looks like this was the result of either:', 'exai'); ?></p>
-<ul>
-	<li><?php _e('a mistyped address', 'exai'); ?></li>
-	<li><?php _e('an out-of-date link', 'exai'); ?></li>
-</ul>
-
-<?php get_search_form(); ?>
