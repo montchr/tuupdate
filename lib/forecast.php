ae3e8994fa8891ead9a4f7cf29729650b242e40f
Chris Montgomery <mont.chr@gmail.com>
2014-02-07 15:52:33 -0500

    build temp bar for widget

--- lib/forecast.php
+++ lib/forecast.php
@@ -72,27 +72,41 @@ function tuupdate_get_compass_direction($bearing) {
 }
 
 
-function tuupdate_weather_widget($container_class = 'forecast-widget', $nextdays_num = 3) {
+/**
+ * Forecast.io weather widget
+ *
+ * Generates a Forecast.io weather widget based
+ * heavily on DuckDuckGo's JS widget.
+ *
+ * @link  https://github.com/duckduckgo/zeroclickinfo-spice/tree/master/share/spice/forecast DuckDuckGo Forecast.io JS widget
+ *
+ * @param  string  $container_class The wrapper class for the widget. Defaults to `forecast-widget`.
+ * @param  integer $daily_num       The number of days to display. Defaults to 3. Maximum is 7.
+ * @param  integer $max_temp_height The maximum height for the temperature bars. Default is 84 (px) (24px*3.5)
+ *
+ * @return html                   The widget
+ */
+function tuupdate_weather_widget($container_class = 'forecast-widget', $daily_num = 3, $max_temp_height = 84) {
     global $api_key, $latitude, $longitude, $forecast;
 
     $output = '';
     // Get conditions for next week, including today
-    $conditions_week = $forecast->getForecastWeek($latitude, $longitude);
+    $week = $forecast->getForecastWeek($latitude, $longitude);
     // Get current conditions
-    $conditions_current = $forecast->getCurrentConditions($latitude, $longitude);
+    $current = $forecast->getCurrentConditions($latitude, $longitude);
     // Get hourly conditions
-    $conditions_hourly = $forecast->getForecastToday($latitude, $longitude);
+    $hourly = $forecast->getForecastToday($latitude, $longitude);
 
     // Current conditions variables
-    $current_icon       = $conditions_current->getIcon();
-    $current_temp       = round($conditions_current->getTemperature());
-    $current_summary    = $conditions_current->getSummary();
-    $current_wind_speed = round($conditions_current->getWindSpeed());
-    $current_wind_dir   = tuupdate_get_compass_direction($conditions_current->getWindBearing());
+    $current_icon       = $current->getIcon();
+    $current_temp       = round($current->getTemperature());
+    $current_summary    = $current->getSummary();
+    $current_wind_speed = round($current->getWindSpeed());
+    $current_wind_dir   = tuupdate_get_compass_direction($current->getWindBearing());
 
     // Hourly conditions variables
-    $hour_now           = $conditions_hourly[0];
-    $hour_next          = $conditions_hourly[1];
+    $hour_now           = $hourly[0];
+    $hour_next          = $hourly[1];
     $hour_now_temp      = round($hour_now->getTemperature());
     $hour_next_temp     = round($hour_next->getTemperature());
     // Temperature change direction
@@ -110,31 +124,51 @@ function tuupdate_weather_widget($container_class = 'forecast-widget', $nextdays
             <div class="summary">' . $current_summary . '</div>
             <div class="wind">Wind: ' . $current_wind_speed . ' mph (' . $current_wind_dir . ')</div>
         </div><!-- /.forecast-widget__currently
-        --><div class="forecast-widget__nextdays  one-half  grid__item">';
+        --><div class="forecast-widget__daily  one-half  grid__item">';
 
-    // Show next `$nextdays_num` days, including today
-    for ( $i = 0; $i < $nextdays_num; $i++ ) :
-        $conditions_day = $conditions_week[$i];
-        $day_name       = $i === 0 ? 'Today' : $conditions_day->getTime('D');
-        $day_icon       = $conditions_day->getIcon();
-        $day_summary    = $conditions_day->getSummary();
-        $day_high       = round($conditions_day->getMaxTemperature());
-        $day_low        = round($conditions_day->getMinTemperature());
+        // Find weekly high and low temps, for scaling
+        $Infinity      = INF;
+        $high_temp     = -$Infinity;
+        $low_temp      = $Infinity;
 
-        $output .= '
-            <div class="forecast-widget__nextdays__day">
+        for ( $i = 0; $i < $daily_num; $i++ ) :
+            $day                  = $week[$i];
+            $day_high             = $day->getMaxTemperature();
+            $day_low              = $day->getMinTemperature();
+
+            // This increases the temperature range until the end of the loop
+            if ($day_high > $high_temp) $high_temp = $day_high;
+            if ($day_low < $low_temp) $low_temp = $day_low;
+        endfor;
+
+        // Now create each day element
+        $temp_span     = $high_temp - $low_temp;
+
+        // Show next `$daily_num` days, including today
+        for ( $i = 0; $i < $daily_num; $i++ ) :
+            $day                  = $week[$i];
+            $day_name             = $i === 0 ? 'Today' : $day->getTime('D');
+            $day_icon             = $day->getIcon();
+            $day_summary          = $day->getSummary();
+            $day_high             = $day->getMaxTemperature();
+            $day_low              = $day->getMinTemperature();
+            $day_high_round       = round($day_high);
+            $day_low_round        = round($day_low);
+
+            $temp_height          = $max_temp_height * ($day_high - $day_low) / $temp_span;
+            $temp_top             = $max_temp_height * ($high_temp - $day_high) / $temp_span;
+
+            $output .= '
+            <div class="forecast-widget__daily__day" style="position: relative;">
                 <div class="day-name">' . $day_name . '</div>
                 <div class="conditions"><i class="conditions__icon  informative  text-hide  icon  icon--weather  icon-' . $day_icon . '" title="' . $day_summary . '">' . $day_summary . '</i></div>
-                <div class="temp-bar">
-                    <div class="temp-bar__hi">' . $day_high . 'º</div>
-
-
-
-                    <div class="temp-bar__lo">' . $day_low . 'º</div>
+                <div class="temp-bar" style="height: ' . $temp_height . 'px; top: ' . $temp_top . ';">
+                    <span class="temp-bar__hi">' . $day_high_round . 'º</span>
+                    <span class="temp-bar__lo">' . $day_low_round . 'º</span>
                 </div>
             </div>';
-    endfor;
-    $output .= '
+        endfor;
+        $output .= '
         </div>
     </div>
 </div><!-- /.' . $container_class . ' -->';
