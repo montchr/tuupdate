ca086600e4009e4bc10680d8d9f993f0f47a6f5b
Chris Montgomery <mont.chr@gmail.com>
2014-02-13 19:21:16 -0500

    clean up scss imports, remove sidebar dupe
    - reorder imports
    - add comments
    - consolidate icons partials
    - remove improperly merged php template

--- templates/pane-sidebar-weather.php
+++ templates/pane-sidebar-weather.php
@@ -1,9 +1,8 @@
 <div class="sidebar-pane--weather__content--twitter  widgets--weather  widgets">
+
+  <?php echo tuupdate_weather_widget(); ?>
+
   <?php dynamic_sidebar('sidebar-weather'); ?>
-
-    <?php echo tuupdate_weather_widget(); ?>
-
-	<?php dynamic_sidebar('sidebar-weather'); ?>
 
   <a href="//twitter.com/tu_weatherwatch" class="btn  btn--soft  btn--dark  icon-text" target="_blank">
     <i class="icon-twitter  icon-text__icon"></i>Follow @TU_WeatherWatch
