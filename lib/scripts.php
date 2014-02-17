10cdd0e33c69ca741054473d08c6877467d7f82d
Chris Montgomery <mont.chr@gmail.com>
2014-02-17 14:11:27 -0500

    forecast widget clearfix

--- lib/scripts.php
+++ lib/scripts.php
@@ -12,7 +12,7 @@
  * 4. /theme/assets/js/main.js    (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'ad0a29bc0a95623b6d13cafcea4d32a2');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'e90c7eb363899b2f55d362ad27291e58');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
